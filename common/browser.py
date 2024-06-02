0c65aca5d044f0426eb8d7f18330d4ba2adac53d
TH3-F001 <123153982+TH3-F001@users.noreply.github.com>
2024-06-01 20:02:04 -0700

    Refactored cookie workflow, doesnt seem to work for visla, whatever.

--- common/browser.py
+++ common/browser.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
+
 
 from urllib.parse import urlparse
 from time import sleep
@@ -16,6 +18,7 @@ import shutil
 import random
 import pickle
 import tldextract
+import requests
 
 class WebBrowser:
     def __init__(self, wait_time):
@@ -24,22 +27,6 @@ class WebBrowser:
         self.cookie_path = 'appdata/cookies.pkl'
         self.local_storage_path = 'appdata/local_storage.json'
         self.sessions_path = 'appdata/sessions.json'
-
-        # Initialize the Options object
-        self.options = Options()
-        # self.options.headless = True
-        self.options.add_argument("--window-size=1920,1080")
-        self.options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
-        self.options.binary_location = self.find_firefox_binary()
-
-        # Initialize the Service Object
-        self.service = Service(executable_path=self.geckodriver_path)
-
-        # Initialize the WebDriver with the specified options
-        self.driver = webdriver.Firefox(options=self.options, service=self.service)
-
-        # Define the WebDriverWait time
-        self.wait = WebDriverWait(self.driver, wait_time)
 
         #Dictionary used for mapping By enums for data retrieval
         self.by_dict = {
@@ -52,6 +39,10 @@ class WebBrowser:
             "partial_link_text": By.PARTIAL_LINK_TEXT,
             "tag_name": By.TAG_NAME
         }
+        self.driver = None
+
+        self.wait_time = wait_time
+
 
 
 #region Genereal Utilities
@@ -78,19 +69,56 @@ class WebBrowser:
         parsed_url = tldextract.extract(self.driver.current_url)
         root_domain = f"{parsed_url.domain}.{parsed_url.suffix}"
         return root_domain
+
+
+    def url_is_valid(self, url=""):
+        result = False
+        try:
+            response = requests.head(url, allow_redirects=True)
+            result = response.status_code < 400
+        except Exception as e:
+            print("ERROR: URL is blank or invalid")
+
+        return result
     
 #endregion
 
     
 #region Browser Control
+    def initialize_driver(self):
+        print("Initializing Driver...")
+        # Initialize the Options object
+        options = Options()
+        # self.options.headless = True
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument(
+            "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0")
+        options.binary_location = self.find_firefox_binary()
+
+        # Initialize the Service Object
+        service = Service(executable_path=self.geckodriver_path)
+
+        # Initialize the WebDriver with the specified options
+        self.driver = webdriver.Firefox(options=options, service=service)
+        # Define the WebDriverWait time
+        self.wait = WebDriverWait(self.driver, self.wait_time)
+
+
     def goto_url(self, url=""):
-        if url != "":
+        if self.url_is_valid(url):
             try:
+                if self.driver == None:
+                    self.initialize_driver()
+
                 result = self.driver.get(url)
-            except:
-                print("Invalid URL! Try again!")
-                return
-            return result
+                return result
+            except Exception as e:
+                print(f"Error while navigating to URL: {url}")
+                print(e)
+
+        return None
+
+
 
 
     def save_and_close(self):
@@ -112,6 +140,17 @@ class WebBrowser:
 
     def click(self, element: WebElement):
         element.click()
+
+
+    def click_center_screen(self):
+        window_size = self.driver.get_window_size()
+        center_x = window_size['width'] / 2
+        center_y = window_size['height'] / 2
+
+        actions = ActionChains(self.driver)
+        actions.move_by_offset(center_x, center_y).click().perform()
+        actions.move_by_offset(-center_x, -center_y).perform()  # Move back to the origin
+
 
 #endregion
 
@@ -156,17 +195,23 @@ class WebBrowser:
         cookies = self.driver.get_cookies()
         pickle.dump(cookies, open(self.cookie_path, "wb"))
 
+    def load_cookies(self, url):
+        if not os.path.exists(self.cookie_path):
+            print("No cookies found to load.")
+            return
 
-    def load_cookies(self):
         cookies = pickle.load(open(self.cookie_path, "rb"))
-        root_domain = self.get_root_domain(self.driver.current_url)
+        root_domain = self.get_root_domain(url)
+
+        self.driver.get(url)  # Navigate to the domain first
 
         for cookie in cookies:
+            # Only add the cookie if the domain matches
             if root_domain in cookie['domain']:
-                # cookie['domain'] = f".{root_domain}"
                 try:
                     self.driver.add_cookie(cookie)
                 except Exception as e:
+                    print(f"Error adding cookie: {cookie}")
                     print(e)
 
 #endregion
--- common/visla.py
+++ common/visla.py
@@ -12,6 +12,7 @@ class Visla:
         self.login_url = "https://app.visla.us/signin"
         self.browser = WebBrowser(10)
         conf_file = "appdata/visla.yaml"
+        self.root_prompt_file = "appdata/visla_root_prompt.txt"
 
         # Create visla config file if it doesnt already exist
         if not os.path.exists(conf_file):
@@ -30,13 +31,53 @@ class Visla:
         
         # Load Root Prompt file into self.root_prompt
         print("\nLoading visla_root_prompt.txt...")
-        root_prompt_file = "appdata/visla_root_prompt.txt"
-        with open(root_prompt_file, 'r') as file:
-            self.root_prompt = yaml.safe_load(file)
+
+
         print("\tLoaded.")
         
 
+#region Navigation
+    def sign_in(self):
+        print("\tSigning in...")
+        email = self.conf["creds"]["email"]
+        pw = self.conf["creds"]["password"]
 
+        email_field = self.browser.get_element("name", "email")
+        password_field = self.browser.get_element("name", "password")
+
+        self.browser.type_keys(email_field, email)
+        self.browser.type_keys(password_field, pw)
+        self.browser.type_enter(password_field)
+
+        # Wait for the web page to load
+        self.browser.get_element("id", "Create Video")
+
+        # Click center of the web page to bypass tutorial dialog
+        self.browser.click_center_screen()
+        self.browser.save_cookies()
+
+
+    def log_on(self):
+        print("\nLogging on to visla.com...")
+        self.browser.goto_url(self.home_url)
+        if self.browser.has_cookies(self.home_url):
+            self.browser.load_cookies(self.home_url)
+            self.browser.goto_url(self.home_url)
+
+        self.sign_in()
+
+
+    def create_video(self):
+        root_prompt = self.get_root_prompt()
+        
+
+    def open_homepage(self):
+        self.browser.get(self.home_url)
+        sleep(1)
+#endregion
+
+
+#region Utilities
     def create_config_file(self, path):
         print("\nConfig file not found. Please enter your credentials.")
         email = input("Enter your email: ")
@@ -54,52 +95,17 @@ class Visla:
 
         with open(path, 'w') as file:
             yaml.dump(conf, file)
-        
+
         print("\tConfiguration Complete.")
 
+    def get_root_prompt(self):
+        with open(self.root_prompt_file, 'r') as file:
+            root_prompt = yaml.safe_load(file)
 
-    def cookieless_login(self):
-        print("\tSigning in...")
-        email = self.conf["creds"]["email"]
-        pw = self.conf["creds"]["password"]
-
-        email_field = self.browser.get_element("name", "email")
-        password_field = self.browser.get_element("name", "password")
-
-        self.browser.type_keys(email_field, email)
-        self.browser.type_keys(password_field, pw)
-        self.browser.type_enter(password_field)
-
-        create_vid_btn = self.browser.get_element("id", "Create Video")
-        sleep(5)
-        self.browser.save_cookies()
-
-
-    def log_on(self):
-        print("\nLogging on to visla.com...")
-        self.browser.goto_url(self.home_url)
-        if self.browser.has_cookies(self.home_url):
-            self.browser.load_cookies()
-            print(self.browser.driver.get_cookies())
-        else:
-            self.cookieless_login()
-        # if self.browser.has_cookies(self.home_url):  
-        # #     self.browser.load_cookies(self.login_url)
-        # else:
-        
-            
-            # if create_vid_btn:
-                
+        return root_prompt
+#endregion
 
 
 
-            # sleep(5)
 
-            # self.browser.save_cookies()
-            # self.browser.save_local_storage()
-
-
-    def open_homepage(self):
-        self.browser.get(self.home_url)
-        sleep(1)
         
