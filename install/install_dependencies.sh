c0a3670d99fcbe5783b738eb70438cadc87b951f
TH3-F001 <123153982+TH3-F001@users.noreply.github.com>
2024-06-30 16:20:36 -0700

    refactored install_dependencies +colors.lib

--- install/install_dependencies.sh
+++ install/install_dependencies.sh
@@ -1,93 +1,183 @@
 #!/bin/bash
+
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 CSV_FILE="$SCRIPT_DIR/dependencies.csv"
+source "${SCRIPT_DIR}/colors.lib"
 
-# Detect the installed package manager and set the appropriate commands for update and install.
-detect_and_set_commands() {
-    local header_read=0  # Flag to check if the header has been processed.
-    while IFS=',' read -ra CSV_LINE; do
-        if [[ $header_read -eq 0 ]]; then
-            # Process the header to determine which package manager is available on this system.
-            for i in "${!CSV_LINE[@]}"; do
-                # Check if the command for the package manager exists on the system.
-                if command -v "${CSV_LINE[i]}" &> /dev/null; then
-                    PKG_MANAGER="${CSV_LINE[i]}" # Assign the package manager
-                    PKG_MANAGER_INDEX="$i"
-                    break
-                fi
-            done
-            if [[ -z "$PKG_MANAGER" ]]; then
-                echo "No supported package manager found. Exiting."
-                exit 1
-            fi
-            header_read=1  # Set the header as read. so we can continue (below)
-        else
-            # Process package commands based on the header information.
-            case "${CSV_LINE[0]}" in
-                update)
-                    # Set the command to update the package repository.
-                    UPDATE_CMD="${CSV_LINE[$PKG_MANAGER_INDEX]}"
-                    ;;
-                install)
-                    # Set the base command to install packages.
-                    INSTALL_CMD="${CSV_LINE[$PKG_MANAGER_INDEX]}"
-                    ;;
-                *)
-                    # Add the correct install command for each package.
-                    local package_install_command="${CSV_LINE[$PKG_MANAGER_INDEX]}"
-                    if [[ -n "$package_install_command" && "$package_install_command" != "[OVERRIDE]" ]]; then
-                        if [[ "$package_install_command" == *"[OVERRIDE]"* ]]; then
-                            # Handle override commands by removing the '[OVERRIDE]' tag and using the command directly.
-                            package_install_command="${package_install_command//\[OVERRIDE\]/}"
-                            INSTALL_COMMANDS+=("$package_install_command")
-                        else
-                            # Normal package installations prepend the install command.
-                            INSTALL_COMMANDS+=("$INSTALL_CMD $package_install_command")
-                        fi
-                    fi
-                    ;;
-            esac
+INSTALL_COMMANDS=()
+PACKAGES=()
+INSTALLED_PACKAGES=()
+UPDATE_CMD=""
+INSTALL_CMD=""
+CHECK_CMD=""
+PKG_MANAGER=""
+PKG_MANAGER_INDEX=""
+
+# Get Dependency Data From CSV
+detect_package_manager() {
+    echo -e "\tDetermining Package Manager..."
+    IFS=',' read -ra CSV_HEADER < "$CSV_FILE"
+
+    for i in "${!CSV_HEADER[@]}"; do
+        if [[ "${CSV_HEADER[i]}" != "package" ]] && command -v "${CSV_HEADER[i]}" &> /dev/null; then
+            PKG_MANAGER="${CSV_HEADER[i]}"
+            PKG_MANAGER_INDEX="$i"
+            echo -e "${GREEN}\t\tPackage manager found: $PKG_MANAGER${NC}"
+            return
         fi
-    done < "$CSV_FILE"
+    done
+
+    echo -e "${YELLOW}\tERROR: No supported package manager found. Please install the following dependencies manually:"
+    list_dependencies
+    echo -e "${NC}"
+    exit 1
 }
 
-# Update the package manager using the command specified in the CSV.
-update_package_manager() {
-    echo "Yo! We need sudo access to install your dependencies!"
+list_dependencies() {
+    awk -F',' '!/^(update|install|check_installed|package)/{print $1}' "$CSV_FILE"
+}
 
-    # echo for sudo caching
-    sudo echo "" >/dev/null
-    if [ $? -eq 0 ]; then
-        # If the sudo command succeeded, proceed with the update
-        echo "Updating package manager with command: $UPDATE_CMD"
-        eval "$UPDATE_CMD"
+retrieve_package_manager_commands() {
+    local commands=(update install check_installed)
+    for command in "${commands[@]}"; do
+        IFS=',' read -ra CSV_LINE < <(awk -v cmd="$command" -F',' '$1 == cmd {print $0}' "$CSV_FILE")
+        case "$command" in
+            update) UPDATE_CMD="${CSV_LINE[$PKG_MANAGER_INDEX]}" ;;
+            install) INSTALL_CMD="${CSV_LINE[$PKG_MANAGER_INDEX]}" ;;
+            check_installed) CHECK_CMD="${CSV_LINE[$PKG_MANAGER_INDEX]}" ;;
+        esac
+    done
+}
+
+parse_package_rows() {
+    while IFS=',' read -ra CSV_LINE; do
+        local packages_to_install="${CSV_LINE[$PKG_MANAGER_INDEX]}"
+        if [[ -n "$packages_to_install" && "$packages_to_install" != "[OVERRIDE]" ]]; then
+            if [[ "$packages_to_install" == *"[OVERRIDE]"* ]]; then
+                INSTALL_COMMANDS+=("${packages_to_install//\[OVERRIDE\]/}")
+            else
+                PACKAGES+=("$packages_to_install")
+                INSTALL_COMMANDS+=("$INSTALL_CMD $packages_to_install")
+            fi
+        fi
+    done < <(awk -F',' '!/^(update|install|check_installed|package)/' "$CSV_FILE")
+}
+
+update_package_manager() {
+    echo -e "\tUpdating package manager with command: $UPDATE_CMD"
+    eval "$UPDATE_CMD"
+}
+
+execute_install_commands() {
+    echo "Running installation commands..."
+    local new_packages=()
+    for cmd in "${INSTALL_COMMANDS[@]}"; do
+        echo "Executing: $cmd"
+        if eval "$cmd"; then
+            local package_name=$(echo "$cmd" | awk '{print $NF}')
+            new_packages=()
+            for pkg in "${PACKAGES[@]}"; do
+                if [[ "$pkg" != "$package_name" ]]; then
+                    new_packages+=("$pkg")
+                else
+                    INSTALLED_PACKAGES+=("$package_name")
+                fi
+            done
+            PACKAGES=("${new_packages[@]}")
+        else
+            echo -e "${RED}Failed to install $cmd${NC}"
+        fi
+    done
+}
+
+filter_out_installed_packages() {
+    split_packages
+    local packages_to_install=()
+    for package in "${PACKAGES[@]}"; do
+        if ! eval "$CHECK_CMD $package" &>/dev/null; then
+            packages_to_install+=("$package")
+            INSTALL_COMMANDS+=("$INSTALL_CMD $package")
+        else
+            INSTALLED_PACKAGES+=("$package")
+        fi
+    done
+    PACKAGES=("${packages_to_install[@]}")
+}
+
+split_packages() {
+    local new_packages=()
+    for item in "${PACKAGES[@]}"; do
+        for pkg in $item; do
+            new_packages+=("$pkg")
+        done
+    done
+    PACKAGES=("${new_packages[@]}")
+}
+
+final_package_check() {
+    if [[ ${#INSTALLED_PACKAGES[@]} -gt 0 ]]; then
+        echo -e "${GREEN}The following dependencies were installed successfully:${NC}"
+        for pkg in "${INSTALLED_PACKAGES[@]}"; do
+            echo -e "${CYAN}- $pkg${NC}"
+        done
+        echo ""
+    fi
+
+    if [[ ${#PACKAGES[@]} -gt 0 ]]; then
+        echo -e "${YELLOW}Some dependencies failed to install. Please install the following packages manually:${NC}"
+        for pkg in "${PACKAGES[@]}"; do
+            echo -e "${RED}- $pkg${NC}"
+        done
     else
-        # If the sudo command failed, exit the script
-        echo "Failed to gain sudo access. Exiting."
-        exit 1
+        if [[ ${#INSTALLED_PACKAGES[@]} -gt 0 ]]; then
+            echo -e "${GREEN}All dependencies installed successfully!${NC}"
+        else
+            echo -e "${GREEN}No dependencies needed installation.${NC}"
+        fi
     fi
 }
 
-# Execute all generated installation commands.
-execute_install_commands() {
-    echo "Running installation commands..."
-    for cmd in "${INSTALL_COMMANDS[@]}"; do
-        echo "Executing: $cmd"
-        eval "$cmd"
-    done
-}
-
-# Print all generated installation commands for review.
+#region Debug
 print_install_commands() {
-    echo "Installation commands to be executed:"
+    echo -e "Installation commands to be executed:"
     for cmd in "${INSTALL_COMMANDS[@]}"; do
-        echo "$cmd"
+        echo -e "\t$cmd"
     done
 }
 
-# Initialize the commands array and start the main process flow.
-INSTALL_COMMANDS=()
-detect_and_set_commands
-#print_install_commands  # Switch to execute_install_commands to run the installation instead of printing.
-update_package_manager
-execute_install_commands
+print_packages() {
+    echo "Packages to be installed:"
+    for cmd in "${PACKAGES[@]}"; do
+        echo -e "\t$cmd"
+    done
+
+    echo "Installed Packages:"
+    for cmd in "${INSTALLED_PACKAGES[@]}"; do
+        echo -e "\t$cmd"
+    done
+
+}
+
+print_batch_debug() {
+    print_install_commands
+    print_packages
+    echo "Install CMD: $INSTALL_CMD"
+    echo "Update CMD: $UPDATE_CMD"
+    echo "Check CMD: $CHECK_CMD"
+    echo "Package Manager: $PKG_MANAGER"
+}
+#endregion
+
+initialize_and_run() {
+    echo -e "${CYAN}Installing Dependencies...${NC}"
+    detect_package_manager
+    retrieve_package_manager_commands
+    parse_package_rows
+    filter_out_installed_packages
+    print_batch_debug
+#    update_package_manager
+#    execute_install_commands
+#    final_package_check
+}
+
+initialize_and_run
--- install/setup.sh
+++ install/setup.sh
@@ -1,15 +1,39 @@
 #!/bin/bash
 
 # This is the development setup script. the installer will run this,
-# is just the bare minimum setup needed to get the project working
+# it's just the bare minimum setup needed to get the project working
 
-# Constants
+
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
+source "$SCRIPT_DIR/colors.lib"
+
+# Script Intro
+echo -e "Setting Up Project Aesop...${NC}"
+
+# Cache Sudo Creds
+echo -e "${CYAN} \tRequesting sudo access for installation:${NC}"
+sudo echo "" >/dev/null
+if [ $? -ne 0 ]; then
+    echo -e "${RED} \t\tSudo request failed. Exiting...${NC}"
+    exit 1
+fi
+
+# Relative Directories
+echo -e "${NC}\tGetting Relative FilePaths..."
+PROJECT_DIR=$(dirname "$SCRIPT_DIR")
+BUILD_CFG_PATH="$PROJECT_DIR/data/config/"
+echo -e "${GREEN}\t\tDone!${NC}"
+
 
 # Install Dependencies
+echo -e "${NC}\tInstalling Dependencies..."
 $SCRIPT_DIR/install_dependencies.sh
+if [ $? -ne 0 ]; then
+  echo -e "${RED}Error: Failed to install Dependencies.${NC}"
+fi
 
-# ToDo Get directory paths
+
+# Get directory paths
 
 
 ## Create and activate python venv
