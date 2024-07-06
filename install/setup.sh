ee05c4c3038d3020b54b9a4754f9182a0a361d66
TH3-F001 <123153982+TH3-F001@users.noreply.github.com>
2024-07-05 22:36:54 -0700

    attempt at making print statements more cohesive

--- install/setup.sh
+++ install/setup.sh
@@ -13,25 +13,25 @@ elif [ "$1" == "-q" ]; then
     AESOP_SETUP_VERBOSITY=0
 fi
 export AESOP_SETUP_VERBOSITY
-print_debug "Verbosity: $AESOP_SETUP_VERBOSITY"
+
 
 # Script Intro
 print_title "Setting Up Project Aesop...\n"
 
 # Cache Sudo Creds
-print_info "Requesting sudo access for installation..."
+print_info "\nRequesting sudo access for installation..."
 sudo echo "" >/dev/null
 if [ $? -ne 0 ]; then
-    exit_error "\tSudo request failed. Exiting..."
+    exit_error "Sudo request failed. Exiting..."
 fi
 
 # Make all scripts executable
-print_info "\tMaking scripts exectuable..."
+print_info "\nMaking scripts exectuable..."
 chmod +x "$SCRIPT_DIR/"*.sh
 
 
 # Install Dependencies
-print_info "\tInstalling Dependencies..."
+echo -e "\n"
 $SCRIPT_DIR/install_dependencies.sh
 if [ $? -ne 0 ]; then
     exit_error "Failed to install Dependencies."
@@ -39,7 +39,7 @@ fi
 
 
 # Generate File Structure
-print_info "\tGenerating File Structure..."
+echo -e "\n"
 $SCRIPT_DIR/build_fs.sh
 if [ $? -ne 0 ]; then
     exit_error "Failed to generate file structure."
@@ -47,7 +47,7 @@ fi
 
 
 ## Create and activate python venv
-print_info "\tGenerating Python Virtual Environment..."
+echo -e "\n"
 $SCRIPT_DIR/build_venv.sh
 if [ $? -ne 0 ]; then
     exit_error "Failed to Generate Python Virtual Environment."
